62b4bb5e6f07bcb3f3e13b29d4526d09a563c660
kangswx <996334837@qq.com>
2019-08-12 10:45:11 +0800

    代码优化

--- src/main/java/com/kangswx/springboottaskscheduled/controller/AsyncController.java
+++ src/main/java/com/kangswx/springboottaskscheduled/controller/AsyncController.java
@@ -1,6 +1,8 @@
 package com.kangswx.springboottaskscheduled.controller;
 
 import com.kangswx.springboottaskscheduled.service.AsyncService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,20 +15,28 @@ import java.util.Date;
 @RequestMapping("/api/v1/asynctask")
 public class AsyncController {
 
+    private static final Logger logger = LoggerFactory.getLogger(AsyncController.class);
+
     @Autowired
     private AsyncService asyncService;
 
+    /**
+     * 调用异步任务接口
+     * @throws InterruptedException
+     */
     @GetMapping("excute")
     public void excuteAsync() throws InterruptedException {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date start = new Date();
-        System.out.println("调用异步任务开始"+sdf.format(start));
+        logger.info("调用异步任务开始"+sdf.format(start)
+                +"， 线程名称为"+Thread.currentThread().getName());
         asyncService.firstTack();
         asyncService.secondTask();
         asyncService.thirdTask();
         Date end = new Date();
-        System.out.println("调用异步任务结束"+sdf.format(end));
+        logger.info("调用异步任务结束"+sdf.format(end));
 
-        System.out.println("执行总时间为： " + (end.getTime() - start.getTime()));
+        logger.info("执行总时间为： " + (end.getTime() - start.getTime())
+                +"， 线程名称为"+Thread.currentThread().getName());
     }
 }
