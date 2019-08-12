04c2ca152a63e16128f917f5e0cff78c65de31d6
kangswx <996334837@qq.com>
2019-08-12 10:46:14 +0800

    代码优化

--- src/main/java/com/kangswx/springboottaskscheduled/taskscheduled/DynamicScheduleTask.java
+++ src/main/java/com/kangswx/springboottaskscheduled/taskscheduled/DynamicScheduleTask.java
@@ -3,16 +3,21 @@ package com.kangswx.springboottaskscheduled.taskscheduled;
 import com.alibaba.druid.util.StringUtils;
 import com.kangswx.springboottaskscheduled.entity.Cron;
 import com.kangswx.springboottaskscheduled.mapper.CronMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.scheduling.support.CronTrigger;
+import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-//@Component
+@Component
 public class DynamicScheduleTask implements SchedulingConfigurer {
+
+    private static final Logger logger = LoggerFactory.getLogger(DynamicScheduleTask.class);
 
     @Autowired
     private CronMapper cronMapper;
@@ -24,7 +29,8 @@ public class DynamicScheduleTask implements SchedulingConfigurer {
 
         scheduledTaskRegistrar.addTriggerTask(
                 //1.添加任务内容(Runnable)
-                () -> System.out.println("执行动态定时任务"+ sdf.format(new Date())),
+                () -> logger.info("执行动态定时任务"+ sdf.format(new Date())
+                        + ", 线程名为： "+Thread.currentThread().getName()),
                 //2.设置执行周期(Trigger)
                 triggerContext -> {
                     //2.1 从数据库获取执行周期
@@ -32,7 +38,7 @@ public class DynamicScheduleTask implements SchedulingConfigurer {
                     String cron_str = cron.getCron();
                     //2.2 合法性校验.
                     if(StringUtils.isEmpty(cron_str)){
-                        System.out.println("获取定时任务执行时间出错");
+                        logger.info("获取定时任务执行时间出错");
                     }
                     //2.3 返回执行周期(Date)
                     return new CronTrigger(cron_str).nextExecutionTime(triggerContext);
