ba2707d7c7bee9f26358d3fb5deace5ca37e98f1
kangswx <996334837@qq.com>
2019-08-12 10:45:38 +0800

    代码优化

--- src/main/java/com/kangswx/springboottaskscheduled/service/impl/AsyncServiceImpl.java
+++ src/main/java/com/kangswx/springboottaskscheduled/service/impl/AsyncServiceImpl.java
@@ -1,6 +1,8 @@
 package com.kangswx.springboottaskscheduled.service.impl;
 
 import com.kangswx.springboottaskscheduled.service.AsyncService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -10,29 +12,37 @@ import java.util.Date;
 @Service
 public class AsyncServiceImpl implements AsyncService {
 
+    private static final Logger logger = LoggerFactory.getLogger(AsyncServiceImpl.class);
+
     private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     @Async
     @Override
     public void firstTack() throws InterruptedException {
-        System.out.println("第一个异步任务开始执行: " + sdf.format(new Date()) + " 线程名为： " + Thread.currentThread().getName());
+        logger.info("第一个异步任务开始执行: " + sdf.format(new Date())
+                + " 线程名为： " + Thread.currentThread().getName());
         Thread.sleep(1000 * 3L);
-        System.out.println("第一个异步任务执行结束: " + sdf.format(new Date()) + " 线程名为： " + Thread.currentThread().getName());
+        logger.info("第一个异步任务执行结束: " + sdf.format(new Date())
+                + " 线程名为： " + Thread.currentThread().getName());
     }
 
     @Async
     @Override
     public void secondTask() throws InterruptedException {
-        System.out.println("第二个异步任务开始执行: " + sdf.format(new Date()) + " 线程名为： " + Thread.currentThread().getName());
+        logger.info("第二个异步任务开始执行: " + sdf.format(new Date())
+                + " 线程名为： " + Thread.currentThread().getName());
         Thread.sleep(1000 * 4L);
-        System.out.println("第二个异步任务执行结束: " + sdf.format(new Date()) + " 线程名为： " + Thread.currentThread().getName());
+        logger.info("第二个异步任务执行结束: " + sdf.format(new Date())
+                + " 线程名为： " + Thread.currentThread().getName());
     }
 
     @Async
     @Override
     public void thirdTask() throws InterruptedException {
-        System.out.println("第三个异步任务开始执行: " + sdf.format(new Date()) + " 线程名为： " + Thread.currentThread().getName());
+        logger.info("第三个异步任务开始执行: " + sdf.format(new Date())
+                + " 线程名为： " + Thread.currentThread().getName());
         Thread.sleep(1000 * 5L);
-        System.out.println("第三个异步任务执行结束: " + sdf.format(new Date()) + " 线程名为： " + Thread.currentThread().getName());
+        logger.info("第三个异步任务执行结束: " + sdf.format(new Date())
+                + " 线程名为： " + Thread.currentThread().getName());
     }
 }
